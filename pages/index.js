4520debeb0fcb3e6bcdddbaabcf43f37eae4fa3f
devtopher <christopher.david.mccoy@gmail.com>
2022-02-05 00:11:00 -0500

    trying to get speed change to work on apple eating json updated for different npm -v

--- pages/index.js
+++ pages/index.js
@@ -4,22 +4,27 @@ import Grid from "../components/Grid";
 const width = 10;
 const INITIAL_GAME_STATE = {
   direction: 1,
-  snake: [0,1,2],
+  snake: [0, 1, 2],
   score: 0,
   apple: 27,
   born: false,
-  isDead: true
+  isDead: true,
+  // speed: 1000
 }
-
-
+//tried everything I could think of to get the speed update to work but no dice
+let speed = 1000
+let speedToggle = false
+if (speedToggle) {
+  speed = speed * 0
+}
 export default function Home() {
   const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
   const [gameLoopId, setGameLoopId] = useState(-1);
 
   const gameLoop = () => {
     setGameState((currentGameState) => {
-      const { snake, direction, score } = currentGameState;
-      console.log("Looping", direction, snake, score);
+      const { snake, direction, score, speed } = currentGameState;
+      console.log("Looping", direction, snake, score, speed);
 
       const currentSnakeHead = snake[snake.length - 1];
       const nextSnakeHead = currentSnakeHead + direction;
@@ -27,57 +32,45 @@ export default function Home() {
 
       let isDead = currentGameState.isDead;
       let apple = currentGameState.apple;
+      // let speed = currentGameState.speed;
       function generateApples() {
         console.log("generating new apple")
         let newApple = -1;
-      
-        do {newApple = Math.floor(Math.random() * 100)}
+        speedToggle = true
+
+        do { newApple = Math.floor(Math.random() * 100) }
         while (snake.includes(newApple))
-        
+
         apple = newApple;
       }
-      
 
       if (((direction === 1) && (currentSnakeHead % width === width - 1)) ||
-          ((direction === -1) && (currentSnakeHead % width === 0)) ||
-          ((direction === width) && (currentSnakeHead + width >= (width * width))) ||
-          ((direction === -width) && (currentSnakeHead - width <= 0)) ||
-           (snake.includes(nextSnakeHead))
+        ((direction === -1) && (currentSnakeHead % width === 0)) ||
+        ((direction === width) && (currentSnakeHead + width >= (width * width))) ||
+        ((direction === -width) && (currentSnakeHead - width <= 0)) ||
+        (snake.includes(nextSnakeHead))
       ) {
         console.log("snake is dead");
         isDead = true
-      } else {//need to write the logic for whether I ate the apple and remember to change it back to false after the snake updates once.
-        
-        const iAteTheApple = false;
-        if (snake.includes(apple)){
-          iAteTheApple = true
+      } else if (snake.includes(apple)) {
+        generateApples()
+        score++
+        // setSpeed(speed*0)
+        // speed = speed * 0
+        console.log(speed)
+        nextSnake = [];
+        for (let i = 0; i <= snake.length - 1; i++) {
+          nextSnake.push(snake[i]);
         }
-        if (iAteTheApple) {
-          generateApples()
-          score ++
-          // moar score
-          // new apple
-          // this line below and through 71 determines what to do if the apple was eaten versus if it was not eaten.
-        } if (iAteTheApple){
-            nextSnake = [];
+        nextSnake.push(nextSnakeHead);
+      }
+      else {
+        nextSnake = [];
 
-          for (let i = 0; i <= snake.length - 1; i++) {
-            nextSnake.push(snake[i]);
-          }
-    
-          nextSnake.push(nextSnakeHead);
-          }
-          else {
-                nextSnake = [];
-
-            for (let i = 1; i <= snake.length - 1; i++) {
-              nextSnake.push(snake[i]);
-            }
-      
-            nextSnake.push(nextSnakeHead);
-              }
-          
-          
+        for (let i = 1; i <= snake.length - 1; i++) {
+          nextSnake.push(snake[i]);
+        }
+        nextSnake.push(nextSnakeHead);
       }
 
       return {
@@ -85,7 +78,8 @@ export default function Home() {
         apple,
         isDead,
         snake: nextSnake,
-        score
+        score,
+        // speed
       };
     });
   };
@@ -99,7 +93,15 @@ export default function Home() {
     });
   }
 
-  
+  // function setSpeed(speed) {
+  //   setGameState((currentGameState) => {
+  //     return {
+  //       ...currentGameState,
+  //       speed
+  //     }
+  //   })
+  // }
+
 
   function setDirection(direction) {
     setGameState((currentGameState) => {
@@ -126,8 +128,9 @@ export default function Home() {
     document.addEventListener("keydown", keyHandler);
 
     if (!gameState.isDead && gameLoopId === -1) {
-      const newGameLoopId = setInterval(gameLoop, 1000);
-
+      clearInterval(gameLoopId)
+      const newGameLoopId = setInterval(gameLoop, speed);
+      
       setGameLoopId(newGameLoopId);
     } else if (gameState.isDead && gameLoopId !== -1) {
       console.log("game loop done")
@@ -143,13 +146,14 @@ export default function Home() {
   return <>
     {gameState.direction} - {JSON.stringify(gameState.snake)} - Is dead: {gameState.isDead}
     <br />
-    <Grid 
-      snake={gameState.snake} 
-      score={gameState.score} 
-      dead={gameState.isDead} 
+    <Grid
+      snake={gameState.snake}
+      score={gameState.score}
+      dead={gameState.isDead}
       apple={gameState.apple}
       born={gameState.born}
-      width={width} 
+      width={width}
+      speed={gameState.speed}
       startGame={startGameLoop} />
   </>
 }
